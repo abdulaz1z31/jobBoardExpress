e10c25ac04b404843c2818459d14aebef19b1383
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-12-01 23:13:48 +0500

    commit 1

--- src/utils/logger.utils.js
+++ src/utils/logger.utils.js
@@ -1,26 +1,11 @@
-// import { createLogger, transports, format } from 'winston'
-
-// export const logger = createLogger({
-//     level: 'silly',
-//     format: format.combine(
-//         format.timestamp(),
-//         format.json(),
-//         format.colorize({ all: true }),
-//     ),
-//     transports: [
-//         new transports.Console(),
-//         new transports.File({ filename: 'application.log' }),
-//     ],
-// })
-
 import { createLogger, transports, format } from 'winston'
 
 export const logger = createLogger({
-    level: 'info',
+    level: 'silly',
     format: format.combine(
         format.timestamp(),
+        format.json(),
         format.colorize({ all: true }),
-        format.simple(),
     ),
     transports: [
         new transports.Console(),
