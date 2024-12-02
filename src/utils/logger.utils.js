5952d32e8616662edf5cd1af470186091ad6f976
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2024-12-02 14:21:20 +0500

    added cauth exepticon

--- src/utils/logger.utils.js
+++ src/utils/logger.utils.js
@@ -12,3 +12,13 @@ export const logger = createLogger({
         new transports.File({ filename: 'application.log' }),
     ],
 })
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err)
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+    process.exit(1)
+})
